c64d682354f9505e9e3e41c7caaf275b9e0f9687
Javier MC <mtzcornejo66@gmail.com>
2023-06-09 00:23:39 -0600

    Init bassic project

---
+++ Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        /// <summary>
+        /// Registers the application services.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
---
+++ Application/AutoMapperFactory.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System.Reflection;
+
+namespace Application;
+public static class AutoMapperFactory
+{
+    public static IConfigurationProvider CreateMapperConfiguration()
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            var assembly = Assembly.GetAssembly(typeof(AutoMapperFactory));
+
+            cfg.AddMaps(assembly);
+        });
+
+        return config;
+    }
+}
